dcc0aed41151f0bacd4fb1ee1592ec4414efb427
Lcfvs <lcf.vs@hotmail.com>
2020-11-19 23:05:41 +0100

    TS types improvement

--- etched.d.ts
+++ etched.d.ts
@@ -4,30 +4,38 @@
  * @licence MIT
  * @see {@link https://github.com/etchedjs/etched} Etched on GitHub
  */
-type Etched = Readonly<null | ThisType<Etched>>;
 
-type IntersectAll<I extends object[]> = I extends [infer T, ...infer R]
-    ? R extends object[]
-        ? T extends object
-            ? T & IntersectAll<R>
-            : T
-        : {}
-    : unknown;
+declare class Empty extends null {}
+
+type Etched = Readonly<Empty & {}>;
+
+type IntersectAll<I extends object[]> = I extends (infer T)[]
+    ? (T extends T ? (x: T) => unknown : never) extends (x: infer U) => unknown
+        ? U
+        : never
+    : never;
+
+export type Model<I extends Etched | null, M extends object[]> = Readonly<
+    Etched &
+    I &
+    Pick<I, keyof I> | IntersectAll<M>>
+
+export type Instance<I extends Model<Etched, []>, M extends object[]> = Readonly<
+    I &
+    Pick<IntersectAll<M>, Extract<keyof I, keyof IntersectAll<M>>> &
+    Omit<I, keyof IntersectAll<M>>
+    >
 
 export declare const etched: Etched;
 
 export declare function model<I extends Etched, M extends object[]>(
     instance: I | null,
     ...mixins: M
-): Readonly<Etched & I & Pick<I, keyof I> | IntersectAll<M>>;
+): Model<I, M>;
 
 export declare function etch<I extends Etched, M extends Partial<I>[]>(
     instance: I,
     ...mixins: M
-): Readonly<
-    I &
-    Omit<I, keyof IntersectAll<M>> &
-    Pick<IntersectAll<M>, Extract<keyof I, keyof IntersectAll<M>>>
-    >;
+): Instance<I, M>;
 
 export declare function etches(model: Etched, instance: unknown): boolean;
